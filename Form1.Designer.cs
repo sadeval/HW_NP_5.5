56ac8d494d4bd4e886fa25201724520247b684f0
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-20 12:29:30 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace GutenbergBookSearchApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.listBoxBooks = new System.Windows.Forms.ListBox();
+            this.pictureBoxCover = new System.Windows.Forms.PictureBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCover)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(12, 12);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(683, 22);
+            this.textBoxSearch.TabIndex = 3;
+            // 
+            // listBoxBooks
+            // 
+            this.listBoxBooks.ItemHeight = 16;
+            this.listBoxBooks.Location = new System.Drawing.Point(12, 40);
+            this.listBoxBooks.Name = "listBoxBooks";
+            this.listBoxBooks.Size = new System.Drawing.Size(400, 388);
+            this.listBoxBooks.TabIndex = 2;
+            this.listBoxBooks.SelectedIndexChanged += new System.EventHandler(this.listBoxBooks_SelectedIndexChanged);
+            // 
+            // pictureBoxCover
+            // 
+            this.pictureBoxCover.Location = new System.Drawing.Point(420, 40);
+            this.pictureBoxCover.Name = "pictureBoxCover";
+            this.pictureBoxCover.Size = new System.Drawing.Size(368, 400);
+            this.pictureBoxCover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCover.TabIndex = 1;
+            this.pictureBoxCover.TabStop = false;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(701, 9);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(87, 25);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.pictureBoxCover);
+            this.Controls.Add(this.listBoxBooks);
+            this.Controls.Add(this.textBoxSearch);
+            this.Name = "Form1";
+            this.Text = "Gutenberg Book Search";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCover)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.ListBox listBoxBooks;
+        private System.Windows.Forms.PictureBox pictureBoxCover;
+        private System.Windows.Forms.Button buttonSearch;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace GutenbergBookSearchApp
+{
+    public partial class Form1 : Form
+    {
+        private const string TopBooksUrl = "https://www.gutenberg.org";
+        private List<Book> books = new List<Book>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTopBooks();
+        }
+
+        private async void LoadTopBooks()
+        {
+            try
+            {
+                var bookList = await GetTopBooksAsync();
+                books = bookList;
+                DisplayBooks(books);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке списка книг: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task<List<Book>> GetTopBooksAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(TopBooksUrl);
+                response.EnsureSuccessStatusCode();
+
+                string html = await response.Content.ReadAsStringAsync();
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+
+                var bookNodes = doc.DocumentNode.SelectNodes("//ol[@class='books']//li[contains(@class, 'booklink')]");
+
+                if (bookNodes == null || !bookNodes.Any())
+                {
+                    throw new Exception("Не удалось найти узлы с книгами. Проверьте структуру HTML.");
+                }
+
+                List<Book> bookList = new List<Book>();
+                foreach (var bookNode in bookNodes)
+                {
+                    var titleNode = bookNode.SelectSingleNode(".//h3/a");
+                    var coverNode = bookNode.SelectSingleNode(".//img");
+
+                    if (titleNode != null && coverNode != null)
+                    {
+                        string title = titleNode.InnerText.Trim();
+                        string bookUrl = "https://www.gutenberg.org" + titleNode.GetAttributeValue("href", "");
+                        string coverUrl = coverNode.GetAttributeValue("src", "");
+
+                        bookList.Add(new Book { Title = title, Url = bookUrl, CoverUrl = coverUrl });
+                    }
+                }
+                return bookList;
+            }
+        }
+
+        private void DisplayBooks(List<Book> booksToDisplay)
+        {
+            listBoxBooks.Items.Clear(); 
+
+            foreach (var book in booksToDisplay)
+            {
+                listBoxBooks.Items.Add(book); 
+            }
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = textBoxSearch.Text.ToLower();
+            var filteredBooks = books.FindAll(b => b.Title.ToLower().Contains(searchText));
+            DisplayBooks(filteredBooks);
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string searchText = textBoxSearch.Text.ToLower();
+            var filteredBooks = books.FindAll(b => b.Title.ToLower().Contains(searchText));
+            DisplayBooks(filteredBooks);
+        }
+
+        private void listBoxBooks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxBooks.SelectedItem is Book selectedBook)
+            {
+                pictureBoxCover.Load(selectedBook.CoverUrl); 
+            }
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string CoverUrl { get; set; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
